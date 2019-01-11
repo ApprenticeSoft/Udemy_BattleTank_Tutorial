557667129e8397a625ec19c9958bbc1076c571ed
ApprenticeSoft <apprentice.soft@gmail.com>
2019-01-11 07:23:57 -0500

    Les tanks enemis peuvent tirer. Ajout d'un délai de rechargement de 3 secondes entre les tirs. Defactorisation du code pour l'IA des tanks enemis.

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -8,13 +8,6 @@
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
-
-	auto PlayerTank = GetPlayerTank();
-	if (!PlayerTank)
-		UE_LOG(LogTemp, Warning, TEXT("AIController can't find player tank"))
-	else
-		UE_LOG(LogTemp, Warning, TEXT("AIController found player %s"), *(PlayerTank->GetName()))
-
 }
 
 void ATankAIController::Tick(float DeltaTime)
@@ -22,21 +15,12 @@ void ATankAIController::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 	//UE_LOG(LogTemp, Warning, TEXT("Player Controller is ticking"));
 
-	if (GetPlayerTank()) {
-		GetControlledTank()->AimAt(GetPlayerTank()->GetActorLocation());
+	auto PlayerTank = Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+	auto ControlledTank = Cast<ATank>(GetPawn());
+
+	if (PlayerTank) {
+		ControlledTank->AimAt(PlayerTank->GetActorLocation());
+
+		ControlledTank->Fire();
 	}
-}
-
-ATank* ATankAIController::GetControlledTank() const 
-{
-	return Cast<ATank>(GetPawn());
-}
-
-ATank * ATankAIController::GetPlayerTank() const
-{
-	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
-	if (!PlayerTank)
-		return nullptr;
-	else
-		return Cast<ATank>(PlayerTank);
 }
